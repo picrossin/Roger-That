dbf7035b59d6c4c3a0b71ea90d561498f202fb84
picrossin <jadengoter@gmail.com>
2021-02-17 17:15:54 -0600

    Added basic VR teleporting.

---
+++ Assets/Scripts/GameQuitter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameQuitter : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
